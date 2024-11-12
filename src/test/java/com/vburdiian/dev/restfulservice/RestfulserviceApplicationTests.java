86fdefe451769b538ba5bdd597ff24e2d2fae556
vburdiian <vburdiian@united-internet.de>
2024-11-12 10:57:24 +0100

    Initial commit

---
+++ src/test/java/com/vburdiian/dev/restfulservice/RestfulserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vburdiian.dev.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
